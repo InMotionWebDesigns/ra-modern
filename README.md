e4b03f362e61f847338c437df477ecd66de11c6b
JustinP1022 <justinp@inmotionhosting.com>
2018-02-27 16:28:15 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RA-Palettes
+- Contributors: ajda99, ciscorey, JimiRadabaugh, justinp1985, rramo012, timph
+- Tags: custom-background, custom-colors, custom-menu, editor-style, responsive-layout, rtl-language-support, translation-ready
+- Requires at least: 4.3
+- Tested up to: 4.9.4
+- License: GNU General Public License v2 or later
+- License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+BoldGrid Child Theme based on [Wedge](https://github.com/BoldGrid/wedge)
---
+++ inc/boldgrid-theme-framework-config/config.php
@@ -0,0 +1,166 @@
+<?php
+function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
+
+	// Text Domain.
+	$boldgrid_framework_configs['theme_name'] = 'ra-palettes';
+
+	// Enable Sticky Footer.
+	$boldgrid_framework_configs['scripts']['boldgrid-sticky-footer'] = true;
+
+	// Enable typography controls.
+	$boldgrid_framework_configs['customizer-options']['typography']['enabled'] = true;
+
+	// Enable attribution links.
+	$boldgrid_framework_configs['temp']['attribution_links'] = true;
+
+	// Enable template wrapper.
+	$boldgrid_framework_configs['boldgrid-parent-theme'] = true;
+
+	// Specify the parent theme's name.
+	$boldgrid_framework_configs['parent-theme-name'] = 'prime';
+
+	// Select the header template to use.
+	$boldgrid_framework_configs['template']['header'] = 'generic';
+
+	// Select the footer template to use.
+	$boldgrid_framework_configs['template']['footer'] = 'generic';
+
+	// Add container to call to action widget.
+	$boldgrid_framework_configs['template']['pages']['global']['call-to-action'] = 'container';
+	
+	// Remove background image.
+	$boldgrid_framework_configs['customizer-options']['background']['defaults']['background_image'] = false;
+	
+	// Assign Locations for Generic Header.
+	$boldgrid_framework_configs['template']['locations']['header'] = array(
+		'1' => array( '[menu]secondary' ),
+		'5' => array( '[widget]boldgrid-widget-1' ),
+		'6' => array( '[action]boldgrid_site_identity' ),
+		'11' => array( '[action]boldgrid_primary_navigation' ),
+	);
+
+	// Assign Locations for Generic Header.
+	$boldgrid_framework_configs['template']['locations']['footer'] = array(
+		'1' => array( '[menu]tertiary' ),
+		'5' => array( '[widget]boldgrid-widget-3', '[menu]footer_center' ),
+		'8' => array( '[action]boldgrid_display_attribution_links' ),
+	);
+
+	// No container is needed on home page for this theme.
+	$boldgrid_framework_configs['template']['pages']['page_home.php']['entry-content'] = '';
+
+	// Enable BoldGrid Color Palette System.
+	$boldgrid_framework_configs['customizer-options']['colors']['enabled'] = true;
+
+	// Set default color palettes for theme.
+	$boldgrid_framework_configs['customizer-options']['colors']['defaults'] = array(
+		array(
+			'default' => true,
+			'format' => 'palette-primary',
+			'neutral-color' => '#e4ecf0',
+			'colors' => array(
+				'#0b151a',
+				'#223e4d',
+				'#396880',
+				'#6298b3',
+				'#b4dcf0',
+			),
+		),
+		array(
+			'format' => 'palette-primary',
+			'neutral-color' => '#f0d9b1',
+			'colors' => array(
+				'#33120f',
+				'#802d26',
+				'#b33f35',
+				'#e67f47',
+				'#ebbc6c',
+			),
+		),
+		array(
+			'format' => 'palette-primary',
+			'neutral-color' => '#ffffff',
+			'colors' => array(
+				'#18181a',
+				'#49494d',
+				'#797980',
+				'#aaaab3',
+				'#e6e6f0',
+			),
+		),
+	);
+
+
+	// Text Contrast Colors.
+	$boldgrid_framework_configs['customizer-options']['colors']['light_text'] = '#ffffff';
+	$boldgrid_framework_configs['customizer-options']['colors']['dark_text'] = '#1a1a1a';
+
+	// Typography Headings.
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['headings_font_family'] = 'Hind';
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['headings_font_size'] = 20;
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['headings_text_transform'] = 'none';
+
+	// Typography Alternate Headings.
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['alternate_headings_font_family'] = 'Hind';
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['alternate_headings_font_size'] = 14;
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['alternate_headings_text_transform'] = 'none';
+
+	$boldgrid_framework_configs['template']['tagline-classes'] = 'h5 alt-font';
+
+	// Typography Navigation
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['navigation_font_family'] = 'Hind';
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['navigation_font_size'] = 14;
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['navigation_text_transform'] = 'uppercase';
+
+	// Typography Body.
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['body_font_family'] = 'Hind';
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['body_font_size'] = 14;
+	$boldgrid_framework_configs['customizer-options']['typography']['defaults']['body_line_height'] = 160;
+
+	// Icons.
+	$boldgrid_framework_configs['social-icons']['size'] = 'large';
+
+	// Menu Locations.
+	$boldgrid_framework_configs['menu']['locations']['secondary'] = 'Above Header';
+	$boldgrid_framework_configs['menu']['locations']['tertiary'] = 'Footer Left';
+	$boldgrid_framework_configs['menu']['footer_menus'][] = 'tertiary';
+
+
+	// Name Widget Areas.
+	$boldgrid_framework_configs['widget']['sidebars']['boldgrid-widget-1']['name'] = 'Above Site Title';
+	$boldgrid_framework_configs['widget']['sidebars']['boldgrid-widget-2']['name'] = 'Call To Action';
+
+	return $boldgrid_framework_configs;
+}
+add_filter( 'boldgrid_theme_framework_config', 'boldgrid_theme_framework_config' );
+
+/**
+ * Site Title & Logo Controls
+ */
+function boldgrid_filter_logo_controls( $controls ) {
+	$controls['logo_font_family']['default'] = 'Hind';
+	$controls['logo_font_size']['default'] = 36;
+	$controls['logo_margin_top']['default'] = 20;
+	$controls['logo_margin_bottom']['default'] = 0;
+
+	return $controls;
+}
+add_filter( 'kirki/fields', 'boldgrid_filter_logo_controls' );
+/**
+ * Open a div for the background on single posts.
+ */
+function boldgrid_single_background_open() {
+	if ( is_single() ) { ?>
+		<div class="row background-primary">
+	<?php }
+}
+add_filter( 'boldgrid_content_before', 'boldgrid_single_background_open' );
+/**
+ * Close the div for single posts.
+ */
+function boldgrid_single_background_close() {
+	if ( is_single() ) { ?>
+		</div>
+	<?php }
+}
+add_filter( 'boldgrid_content_after', 'boldgrid_single_background_close' );
